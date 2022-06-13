30a27f73e3738522d246bdf875168be58158774a
xboyjx <60062969+xboyjx@users.noreply.github.com>
2022-06-13 12:51:47 +0100

    Delete backend/ShoppingListDB directory

--- backend/ShoppingListDB/src/main/java/com/codeclan/example/ShoppingListDB/repositories/FamilyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeclan.example.ShoppingListDB.repositories;
-
-import com.codeclan.example.ShoppingListDB.models.Family;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FamilyRepository extends JpaRepository<Family, Long> {
-}
